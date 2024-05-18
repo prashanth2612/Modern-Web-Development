932ba0ca683228bdb8113a7e38e3b1d829f4e710
prashanth2612 <prashanthit8074@gmail.com>
2024-05-18 12:33:20 +0530

    navbar

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar'
+import './index.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import Placeorder from './pages/Placeorder/Placeorder'
+
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/Cart' element={<Cart/>}/>
+        <Route path='/oredr'element={<Placeorder/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
